cb6a3c1c8c66ccfc50855d37fd31e6d796e09f02
Caesar R. Watts-Hall <93171607+CaesarWattsHall@users.noreply.github.com>
2021-11-03 16:14:45 -0500

    Add files via upload

---
+++ Sort() @Caesarwattshall.py
@@ -0,0 +1,23 @@
+# List of Integers 
+numbers = [1, 4, 6, 7] 
+  
+# Sorting list of Integers 
+numbers.sort(reverse=True) 
+  
+print(numbers) 
+  
+# List of Floating point numbers 
+decimalnumber = [10.08, 19.98, 2.00, 7.31, 3.06] 
+  
+# Sorting list of Floating point numbers 
+decimalnumber.sort(reverse=True) 
+  
+print(decimalnumber) 
+  
+# List of strings 
+words = ["Caesar", "Tyron", "Alejandro", "Behic", "Savior"] 
+  
+# Sorting list of strings 
+words.sort(reverse=True) 
+  
+print(words) 
